23b95cbcf990338a1ede788167de555cc7e542d2
matokoLB <matheuslisboabr@gmail.com>
2022-11-16 10:26:47 -0300

    Informações adicionais

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
    
     return (    
-        <div className="navbar">
-            <div className="navbar-left">
+        <div className="navbar ">
+            <div className="navbar-left ">
                 <Link to="/">
                 </Link>
             </div>
             <div className="topnav" id="myTopnav">
-                <Link to="/about">INFORMAÇÕES</Link>
+                <Link to="/about">INFO</Link>
                 <Link to="/create">CRIAR</Link>
             </div>
         </div>
